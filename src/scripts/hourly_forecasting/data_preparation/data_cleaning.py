a5fc40d2264d1da32a029cf4a394928de4456d25
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2025-04-11 16:32:10 +0100

    Added data preparation functions.

--- src/scripts/hourly_forecasting/data_preparation/data_cleaning.py
+++ src/scripts/hourly_forecasting/data_preparation/data_cleaning.py
@@ -1,2 +1,9 @@
 import pandas as pd
 
+def show_na(data: pd.DataFrame) -> pd.DataFrame:
+
+    pass
+
+def plot_na(data: pd.DataFrame) -> pd.DataFrame:
+
+    pass
--- src/scripts/hourly_forecasting/data_preparation/data_preparation.py
+++ src/scripts/hourly_forecasting/data_preparation/data_preparation.py
@@ -1,5 +1,48 @@
 import pandas as pd
 
-def datetime_conversion(data: pd.DataFrame) -> pd.DataFrame:
+"""
+This file contains date/time conversion and generic data preparation functions.
+"""
 
-    pass
+def create_tz_column(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Creates a time zone indicator column and add it to the data frame.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Original data.
+
+    Returns
+    -------
+    pd.DataFrame
+        New data frame containing the time zone indicator column.
+    """
+
+    new_data = data.copy();
+
+    new_data.loc[new_data.time.str.contains("+01:00", regex=False), "tz_offset"] = 1;
+    new_data.loc[new_data.time.str.contains("+02:00", regex=False), "tz_offset"] = 2;
+
+    return new_data;
+
+def datetime_conversion(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Converts the index to a date/time object.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Original data.
+
+    Returns
+    -------
+    pd.DataFrame
+        Data frame with a date/time index.
+    """
+
+    new_data = data.copy();
+
+    new_data["time"] = pd.to_datetime(new_data["time"], utc=True);
+
+    return new_data;
