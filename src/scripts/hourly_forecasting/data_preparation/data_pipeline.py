eb1e0229b62ef16071d46f69975bb3aeec544bee
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2025-05-02 22:06:11 +0100

    Finished prepare_data_forecasting function.

--- src/scripts/hourly_forecasting/data_preparation/data_pipeline.py
+++ src/scripts/hourly_forecasting/data_preparation/data_pipeline.py
@@ -13,25 +13,28 @@ prepare_data_forecasting
 
 def prepare_data_forecasting(energy_data: pd.DataFrame, weather_data: pd.DataFrame) -> pd.DataFrame:
     """
-    _summary_
+    Executes the data preprocessing pipeline for forecasting tasks.
 
-    _extended_summary_
+    This function executes the entire data preprocessing procedure for forecasting tasks.
+    This includes date/time conversion, the creation of a time zone offset indicator variable,
+    and data frame concatenation operations. Data cleaning, normalisation, and smoothing
+    operations are not included, and will have to be performed using separate functions.
 
     Parameters
     ----------
     energy_data : pd.DataFrame
-        _description_
+        Energy generation data frame.
 
     weather_data : pd.DataFrame
-        _description_
+        Weather features data frame.
 
     Returns
     -------
     pd.DataFrame
-        _description_
+        Data frame formatted for forecasting tasks.
     """
 
-    # Deep copy
+    # Deep copies
     new_energy_data = energy_data.copy();
     new_weather_data = weather_data.copy();
 
@@ -44,12 +47,17 @@ def prepare_data_forecasting(energy_data: pd.DataFrame, weather_data: pd.DataFra
     new_weather_list = [new_weather_data.groupby("city_name").get_group(city) for city in new_weather_data["city_name"].unique()];
     weather_list_no_dups = [df.loc[(~df.index.duplicated()).tolist()] for df in new_weather_list];
     
-    # Concatenate data frames
-    old_names_list = [df.columns.tolist() for df in weather_list_no_dups];
+    # Rename columns
+    old_names_list = [df.columns[1:].tolist() for df in weather_list_no_dups];
     city_names = [df["city_name"].unique().item() for df in weather_list_no_dups];
     new_names_list = [[name + city_name for name in cols] for cols, city_name in zip(old_names_list, city_names)];
-    name_dicts = [[{old_name: new_name} for old_name, new_name in zip(old_cols, new_cols)] for old_cols, new_cols in zip(old_names_list, new_names_list)];
-    weather_list_no_dups_new_cols = [df.rename(name_dict) for df, name_dict in zip(weather_list_no_dups, name_dicts)];
-    new_data = pd.concat(weather_list_no_dups_new_cols, axis=1);
+    name_dicts = [{old_name: new_name for old_name, new_name in zip(old_cols, new_cols)} for old_cols, new_cols in zip(old_names_list, new_names_list)];
+    weather_list_no_dups_new_cols = [df.drop(columns="city_name").rename(columns=name_dict) for df, name_dict in zip(weather_list_no_dups, name_dicts)];
+    
+    # Concatenate weather data frames
+    processed_weather_data = pd.concat(weather_list_no_dups_new_cols, axis=1);
+
+    # Concatenate energy and weather data frames
+    new_data = pd.concat([new_energy_data, processed_weather_data], axis=1);
 
     return new_data;
