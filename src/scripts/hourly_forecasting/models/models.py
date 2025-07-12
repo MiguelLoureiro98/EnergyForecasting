7fa76e9439bd8a9fb369136326ca13010d1373a0
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2025-07-12 18:05:28 +0100

    Documented NaiveSimilarDay model.

--- src/scripts/hourly_forecasting/models/models.py
+++ src/scripts/hourly_forecasting/models/models.py
@@ -16,24 +16,38 @@ class NaiveSimilarDay():
     """
     Implements the naive similarity-based method.
 
-    This class implements Nogales' naive similarity-based method for electricity price forecasting.
-    ...
+    This class implements the naive similar day method first proposed by 
+    Conejo et al ([1](#1)). As no training process takes place, only the 
+    test data need be provided.
 
     Parameters
     ----------
     test_data : pd.DataFrame
-        ...
+        Test dataset.
 
     forecasting_vars : list | str, default="price actual"
-        ...
+        Target forecasting variables.
 
     Methods
     -------
     forecast(initial_point: str)
-        ...
+        Forecast test set values.
 
     initial_data_point(data: pd.DataFrame)
-        ...
+        Static method. Used to determine the string that should be passed to the forecast method.
+
+    Notes
+    -----
+    This method was first proposed by Conejo et al ([1](#1)). Electricity prices for a given day are assumed to be 
+    equal to those for a similar day. Similarity is defined by a simple heuristic rule: a Monday is similar to the 
+    previous Monday; Tuesdays, Wednesdays, Thursdays, and Fridays are similar to the preceding Monday. 
+    This model should only be used as a benchmark to assess the performance of more elaborate forecasting models.
+
+    References
+    ----------
+    <a id="1">[1]</a> Antonio J. Conejo, Javier Contreras, Rosa Espínola, and Miguel A. Plazas.
+    Forecasting electricity prices for a day-ahead pool-based electric energy
+    market. International journal of forecasting, 21(3):435–462, 2005.  
     """
 
     def __init__(self, test_data: pd.DataFrame, forecasting_vars: list | str="price actual") -> None:
@@ -45,19 +59,23 @@ class NaiveSimilarDay():
 
     def forecast(self, initial_point: str) -> pd.DataFrame:
         """
-        _summary_
+        Forecast test set values.
 
-        _extended_summary_
+        This method can used to forecast (future) test set values. The first hour of the first Monday 
+        of the test set must be provided as the initial point, as the model can only start predicting 
+        values from a Monday. The model will not generate forecasts for data preceding the initial point, 
+        although these values will still be used to forecast future values.
 
         Parameters
         ----------
         initial_point : str
-            _description_
+            Date corresponding to the first day whose values should be forecast. The initial_data_point() method 
+            can be used to find this data point automatically.
 
         Returns
         -------
         pd.DataFrame
-            _description_
+            Forecasts.
         """
 
         data = self._data.copy();
@@ -79,19 +97,19 @@ class NaiveSimilarDay():
     @staticmethod
     def initial_data_point(data: pd.DataFrame) -> pd.Timestamp:
         """
-        _summary_
+        Get the initial data point for forecasting purposes.
 
-        _extended_summary_
+        This method can be used to find the initial data point required by the forecast() method.
 
         Parameters
         ----------
         data : pd.DataFrame
-            _description_
+            Test dataset.
 
         Returns
         -------
         pd.Timestamp
-            _description_
+            Timestamp containing the index of the initial data point.
         """
 
         return (data.loc[data.weekday == 0].index[0] + pd.Timedelta(1, "W"));
